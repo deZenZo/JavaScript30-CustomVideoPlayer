f4e9d9aa7f12c45bbaa5e4d01ce886f2db5abe00
deZenzo <kyneandrewhao@gmail.com>
2018-09-11 17:27:42 +0800

    added contents

---
+++ script.js
@@ -0,0 +1,71 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress  = player.querySelector('.progress');
+const toggle = player.querySelector('.toggle');
+const progressBar = player.querySelector('.progress__filled');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+function togglePlay(){
+  if(video.paused){/*video.paused returns true then u can play the video*/
+    video.play();
+  }else{
+    video.pause();
+  }
+}
+let video_functions= {};
+function updateButton(){
+  toggle.textContent = this.paused ? '►' : '❚ ❚';
+}
+
+function skip(){
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+  video[this.name] = this.value;
+}
+
+function handleProgress(){
+  const percent = (video.currentTime / video.duration)*100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e){
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function toggleFullscreen(){
+  if(video.requestFullScreen){
+    video.requestFullScreen();
+  }
+  else if (video.webkitRequestFullScreen) {
+    video.webkitRequestFullScreen();
+  }
+  else if (video.mozRequestFullScreen){
+    video.mozRequestFullScreen();
+  }
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+let mousedown = false;
+progress.addEventListener('click',scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+const flbutton = document.querySelector('.flbutton');
+flbutton.addEventListener('click', toggleFullscreen);
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
